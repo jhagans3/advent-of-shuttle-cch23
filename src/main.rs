7e569297a3c9c3d4b8a9625a5bba332ba6e923e5
James Hagans <jhagans@acm.org>
2023-12-15 13:59:45 -0500

    day 1 task 1
    Signed-off-by: James Hagans <jhagans@acm.org>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::Path,
     http::{StatusCode, Uri},
     routing::get,
     Router,
@@ -15,11 +16,19 @@ async fn neg_one_error(uri: Uri) -> (StatusCode, String) {
     )
 }
 
+async fn cube_the_bits(Path((num1, num2)): Path<(i32, i32)>) -> (StatusCode, String) {
+    let xor = num1 ^ num2;
+    let pow = i32::pow(xor, 3);
+
+    (StatusCode::OK, format!("{pow}"))
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(neg_one_error));
+        .route("/-1/error", get(neg_one_error))
+        .route("/1/:num1/:num2", get(cube_the_bits));
 
     Ok(router.into())
 }
